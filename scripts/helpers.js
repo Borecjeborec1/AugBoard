a0ea228d88f0b7c66b11ee217e80bb674264817f
borecjeborec1 <robingrundel@seznam.cz>
2022-08-18 21:27:59 +0200

    JSON storage fix

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -4,11 +4,14 @@ function closest(arr, x, ind) {
 }
 
 function colorDiff(color1, color2) {
-  return Math.sqrt(
-    Math.pow(color1[0] - color2[0], 2) +
+  // return Math.sqrt(
+  //   Math.pow(color1[0] - color2[0], 2) +
+  //   Math.pow(color1[1] - color2[1], 2) +
+  //   Math.pow(color1[2] - color2[2], 2)
+  // );
+  return (Math.pow(color1[0] - color2[0], 2) +
     Math.pow(color1[1] - color2[1], 2) +
-    Math.pow(color1[2] - color2[2], 2)
-  );
+    Math.pow(color1[2] - color2[2], 2))
 }
 
 function averagePos(positions, ind, based) {
